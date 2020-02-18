d6c46247e0d46cf9c159ba2697e9bd06a557d176
Kirkadlao <Kirkadlao@gmail.com>
2020-02-18 09:06:22 -0500

    initial commit

---
+++ Temperature.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Temperature {
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("");
+        System.out.println("************   Average Temperature Calculator   ************");
+        System.out.println("");
+
+        //Asking user for input
+
+        System.out.println("Please give me the Temperature in Celsius in the past 3 days");
+
+        double temperature1 = sc.nextDouble();
+        double temperature2 = sc.nextDouble();
+        double temperature3 = sc.nextDouble();
+
+        //calculator
+
+        double average = (temperature1 + temperature2 + temperature3)/3;
+
+        System.out.println("The average temperature in the past 3 days is " + average);
+
+    }
+}
